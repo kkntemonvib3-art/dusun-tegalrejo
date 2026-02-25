82988b40d0f0ee35df9f783266401153b75d4240
Kimrizyu <ayubjm01@gmail.com>
2026-02-25 16:33:47 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -395,3 +395,14 @@ if (timelineEl) {
     }).join('');
   }
 }
+
+// NAVBAR TRANSPARAN
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
